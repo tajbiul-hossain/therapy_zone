ed2dbb1b4ab0926a8490a2cdec28667dd50ec3d7
tajbiul-hossain <tbh.nishat@gmail.com>
2020-11-05 01:54:26 +0600

    third commit

--- lib/models/doctorinfo.dart
+++
@@ -1,9 +0,0 @@
-class DoctorInfo {
-  String name;
-  String description;
-  String contactNumber;
-  //declaring variables
-
-  // constructor
-  DoctorInfo({this.name, this.description, this.contactNumber});
-}
--- lib/pages/home/doctor.dart
+++ lib/pages/home/doctor.dart
@@ -1,111 +1,199 @@
-import 'package:therapy_zone/models/doctorinfo.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class Doctor extends StatelessWidget {
-  //DUMMY DOCTOR LIST
+class Doctor extends StatefulWidget {
+  @override
+  _DoctorState createState() => _DoctorState();
+}
 
-  final List<DoctorInfo> doc = [
-    DoctorInfo(
-        name: 'Dr. Hanif Seddiqui',
-        description: 'Random Hospital1',
-        contactNumber: '+8801933236173'),
-    DoctorInfo(
-        name: 'Dr.Manzurul Kabir Ayon',
-        description: 'Random Hospital2',
-        contactNumber: '+8801521316593'),
-    DoctorInfo(
-        name: 'Dr. Fairooz Azim',
-        description: 'Random Hospital3',
-        contactNumber: '+8801817602086'),
-    DoctorInfo(
-        name: 'Dr. Tajbi-ul Hossain',
-        description: 'Random Hospital4',
-        contactNumber: '+8801705504590'),
-    DoctorInfo(
-        name: 'Dr. Ishmamur Rahman',
-        description: 'Random Hospital5',
-        contactNumber: '+8801930541402'),
+class _DoctorState extends State<Doctor> {
+  var name = [
+    "Dr. Hanif Siddequi",
+    "Dr. Osiur Rahman",
+    "Dr. Anwarul Azim",
+    "Dr. Fairooz Azim",
+    "Dr. Manzur Kabir",
+    "Dr. Ishmamur Rahman",
+    "Dr. Tajbiul Hossain"
   ];
-
-  //FUNCTION FOR CALLS
-  // void callDoctor(command) async {
-  //   if (await canLaunch(command)) {
-  //     await launch(command);
-  //   } else {
-  //     print('could not call doctor');
-  //   }
-  // }
+  var info = [
+    "Random Hospital 1",
+    "Random Hospital 2",
+    "Random Hospital 3",
+    "Random Hospital 4",
+    "Random Hospital 5",
+    "Random Hospital 6",
+    "Random Hospital 7"
+  ];
+  var contact = [
+    "01975937956",
+    "01875937956",
+    "01775937956",
+    "01575937956",
+    "01675937956",
+    "01575937956",
+    "01675937956"
+  ];
+  var email = [
+    "hanif@cu.ac.bd",
+    "osi@cu.ac.bd",
+    "azim@cu.ac.bd",
+    "fairoozazim97@gmail.com",
+    "trnwrckd@gmail.com",
+    "ayoncsecu17@gmail.com",
+    "tbh.nishat@gmail.com"
+  ];
+  var img = [
+    "assets/male.png",
+    "assets/male.png",
+    "assets/male.png",
+    "assets/fem.png",
+    "assets/male2.png",
+    "assets/male2.png",
+    "assets/male2.png"
+  ];
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width * 0.6;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color.fromRGBO(99, 43, 108, 1),
         title: Center(
-            child: Padding(
-          padding: const EdgeInsets.fromLTRB(0, 0, 55, 0),
-          child: Text(
-            'Make an appointment',
-            style: TextStyle(
-              color: Color.fromRGBO(252, 195, 163, 1),
-            ),
+            child: Text(
+          'Make an appointment',
+          style: TextStyle(
+            color: Color.fromRGBO(252, 195, 163, 1),
           ),
         )),
       ),
       body: Container(
         color: Color.fromRGBO(39, 15, 54, 1),
         child: ListView.builder(
-          itemCount: doc.length,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.fromLTRB(5, 10, 5, 10),
-              child: Card(
-                color: Color.fromRGBO(99, 43, 108, 5),
-                child: ListTile(
-                    title: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    FlatButton.icon(
-                        icon: Icon(
-                          Icons.phone,
-                          color: Colors.white,
-                        ),
-                        label: Text(
-                          '',
-                          style: TextStyle(
-                            color: Colors.white54,
+            itemCount: name.length,
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                onTap: () {
+                  showDialogFunc(context, img[index], name[index], info[index],
+                      contact[index], email[index]);
+                },
+                child: Card(
+                    color: Color.fromRGBO(39, 15, 54, 1),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 5.0),
+                      child: Row(
+                        children: [
+                          Container(
+                            width: 80,
+                            height: 80,
+                            child: Image.asset(img[index]),
                           ),
-                        ),
-                        onPressed: () {
-                          launch('tel: ${doc[index].contactNumber}');
-                        }),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          doc[index].name,
-                          style: TextStyle(
-                            fontSize: 22,
-                            color: Colors.red[100],
-                          ),
-                        ),
-                        Text(
-                          doc[index].description,
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: Colors.red[100],
-                          ),
-                        )
-                      ],
-                    ),
-                  ],
-                )),
-              ),
-            );
-          },
-        ),
+                          Padding(
+                            padding: const EdgeInsets.all(25.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  name[index],
+                                  style: TextStyle(
+                                    fontSize: 20.0,
+                                    color: Colors.grey,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(height: 10.0),
+                                Container(
+                                  width: width,
+                                  child: Text(
+                                    info[index],
+                                    style: TextStyle(
+                                      fontSize: 15.0,
+                                      color: Colors.grey[500],
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    )),
+              );
+            }),
       ),
     );
   }
+}
+
+void customLaunch(command) async {
+  if (await canLaunch(command)) {
+    await launch(command);
+  } else {
+    print('could not launch $command');
+  }
+}
+
+showDialogFunc(context, img, name, info, mobile, email) {
+  return showDialog(
+      context: context,
+      builder: (context) {
+        return Center(
+          child: Material(
+            type: MaterialType.transparency,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Color.fromRGBO(39, 15, 54, 1),
+              ),
+              padding: EdgeInsets.all(15.0),
+              width: MediaQuery.of(context).size.width * 0.7,
+              height: 320,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(5),
+                    child: Image.asset(img, width: 150, height: 150),
+                  ),
+                  SizedBox(height: 10.0),
+                  Text(
+                    name,
+                    style: TextStyle(
+                        fontSize: 25.0,
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10.0),
+                  Text(
+                    info,
+                    style: TextStyle(fontSize: 15.0, color: Colors.grey[500]),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 10.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      RaisedButton.icon(
+                        onPressed: () {
+                          customLaunch('tel: $mobile');
+                        },
+                        icon: Icon(Icons.phone),
+                        label: Text('Call'),
+                      ),
+                      RaisedButton.icon(
+                        onPressed: () {
+                          customLaunch(
+                              'mailto:$email?subject=make%20appoinment&body=i%20want%20to%20make%20an%20appointment%20on%20dd/mm/yy%20at%20hh:mm');
+                        },
+                        icon: Icon(Icons.email),
+                        label: Text('Email'),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        );
+      });
 }
